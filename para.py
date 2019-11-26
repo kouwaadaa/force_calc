198063044709deba815b8715deb59539d7df6e3b
Koki Wada <kwada@al.cs.kobe-u.ac.jp>
2019-11-26 17:10:14 +0900

    add remove_tank

--- para.py
+++ para.py
@@ -14,13 +14,14 @@ def para_calc(file_name,force,num,wing,alpha,clm):
             iteration.append(int(data[0]))
             data_num.append(float(data[1]))
 
-    ave = sum(data_num[-num:])/num
-    # ave = sum(data_num[351:450])/num
+    #ave = sum(data_num[-num:])/num
+    ave = sum(data_num[801:900])/num
     print('%s:%f' % (force,ave))
 
     #write excel
+    excel_file = 'remove_tank.xlsx'
 
-    book = openpyxl.load_workbook('hole_original_scheme.xlsx')
+    book = openpyxl.load_workbook(excel_file)
     sheet = book['alpha=' + str(alpha)]
     # if wing == 1.0:
     #     if force == 'Drag':
@@ -43,14 +44,14 @@ def para_calc(file_name,force,num,wing,alpha,clm):
     #         sheet.cell(row=15,column=clm).value = ave
     #     if force == 'Moment':
     #         sheet.cell(row=16,column=clm).value = ave 
-    if wing == 1.5:
+    if wing == 1.0:
         if force == 'Drag':
-            sheet.cell(row=8,column=clm).value = ave
+            sheet.cell(row=14,column=clm).value = ave
         if force == 'Lift':
-            sheet.cell(row=9,column=clm).value = ave
+            sheet.cell(row=15,column=clm).value = ave
         if force == 'Moment':
-            sheet.cell(row=10,column=clm).value = ave                      
-    book.save('hole_original_scheme.xlsx')
+            sheet.cell(row=16,column=clm).value = ave                      
+    book.save(excel_file)
 
 
     return ave
